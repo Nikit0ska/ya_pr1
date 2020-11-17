3c3c47c2deadcb1ed6628abc690b1adc700a7d26
nikit0ska <nik.ryabov2014@Mail.ru>
2020-11-17 05:42:09 +0300

    1

---
+++ main.py
@@ -0,0 +1,317 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'form/form.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.1
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_PianoForm(object):
+    def setupUi(self, PianoForm):
+        PianoForm.setObjectName("PianoForm")
+        PianoForm.resize(621, 486)
+        PianoForm.setMinimumSize(QtCore.QSize(621, 486))
+        self.centralwidget = QtWidgets.QWidget(PianoForm)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton_22 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_22.setGeometry(QtCore.QRect(450, 130, 21, 91))
+        self.pushButton_22.setStyleSheet("border: 2px solid #858585;\n"
+"\n"
+"    background-color:black ;\n"
+"")
+        self.pushButton_22.setText("")
+        self.pushButton_22.setObjectName("pushButton_22")
+        self.pushButton_16 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_16.setGeometry(QtCore.QRect(90, 130, 21, 91))
+        self.pushButton_16.setStyleSheet("border: 2px solid #858585;\n"
+"\n"
+"    background-color:black ;\n"
+"")
+        self.pushButton_16.setText("")
+        self.pushButton_16.setObjectName("pushButton_16")
+        self.pushButton_19 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_19.setGeometry(QtCore.QRect(250, 130, 21, 91))
+        self.pushButton_19.setStyleSheet("border: 2px solid #858585;\n"
+"\n"
+"    background-color:black ;\n"
+"")
+        self.pushButton_19.setText("")
+        self.pushButton_19.setObjectName("pushButton_19")
+        self.pushButton_21 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_21.setGeometry(QtCore.QRect(370, 130, 21, 91))
+        self.pushButton_21.setStyleSheet("border: 2px solid #858585;\n"
+"\n"
+"    background-color:black ;\n"
+"")
+        self.pushButton_21.setText("")
+        self.pushButton_21.setObjectName("pushButton_21")
+        self.pushButton_27 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_27.setGeometry(QtCore.QRect(250, 360, 101, 71))
+        self.pushButton_27.setObjectName("pushButton_27")
+        self.pushButton_17 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_17.setGeometry(QtCore.QRect(170, 130, 21, 91))
+        self.pushButton_17.setStyleSheet("border: 2px solid #858585;\n"
+"\n"
+"    background-color:black ;\n"
+"")
+        self.pushButton_17.setText("")
+        self.pushButton_17.setObjectName("pushButton_17")
+        self.pushButton_9 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_9.setGeometry(QtCore.QRect(340, 130, 41, 151))
+        self.pushButton_9.setStyleSheet("border: 2px solid #8f8f91;\n"
+"\n"
+"    background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
+"                                      stop: 0 #f6f7fa, stop: 1 #dadbde);\n"
+"")
+        self.pushButton_9.setText("")
+        self.pushButton_9.setObjectName("pushButton_9")
+        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_3.setGeometry(QtCore.QRect(100, 130, 41, 151))
+        self.pushButton_3.setStyleSheet("border: 2px solid #8f8f91;\n"
+"\n"
+"    background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
+"                                      stop: 0 #f6f7fa, stop: 1 #dadbde);\n"
+"")
+        self.pushButton_3.setText("")
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.pushButton_24 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_24.setGeometry(QtCore.QRect(530, 130, 21, 91))
+        self.pushButton_24.setStyleSheet("border: 2px solid #858585;\n"
+"\n"
+"    background-color:black ;\n"
+"")
+        self.pushButton_24.setText("")
+        self.pushButton_24.setObjectName("pushButton_24")
+        self.pushButton_20 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_20.setGeometry(QtCore.QRect(330, 130, 21, 91))
+        self.pushButton_20.setStyleSheet("border: 2px solid #858585;\n"
+"\n"
+"    background-color:black ;\n"
+"")
+        self.pushButton_20.setText("")
+        self.pushButton_20.setObjectName("pushButton_20")
+        self.pushButton_8 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_8.setGeometry(QtCore.QRect(300, 130, 41, 151))
+        self.pushButton_8.setStyleSheet("border: 2px solid #8f8f91;\n"
+"\n"
+"    background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
+"                                      stop: 0 #f6f7fa, stop: 1 #dadbde);\n"
+"")
+        self.pushButton_8.setText("")
+        self.pushButton_8.setObjectName("pushButton_8")
+        self.pushButton_10 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_10.setGeometry(QtCore.QRect(380, 130, 41, 151))
+        self.pushButton_10.setStyleSheet("border: 2px solid #8f8f91;\n"
+"\n"
+"    background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
+"                                      stop: 0 #f6f7fa, stop: 1 #dadbde);\n"
+"")
+        self.pushButton_10.setText("")
+        self.pushButton_10.setObjectName("pushButton_10")
+        self.pushButton_14 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_14.setGeometry(QtCore.QRect(540, 130, 41, 151))
+        self.pushButton_14.setStyleSheet("border: 2px solid #8f8f91;\n"
+"\n"
+"    background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
+"                                      stop: 0 #f6f7fa, stop: 1 #dadbde);\n"
+"")
+        self.pushButton_14.setText("")
+        self.pushButton_14.setObjectName("pushButton_14")
+        self.pushButton_26 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_26.setGeometry(QtCore.QRect(480, 360, 101, 71))
+        self.pushButton_26.setObjectName("pushButton_26")
+        self.pushButton_7 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_7.setGeometry(QtCore.QRect(260, 130, 41, 151))
+        self.pushButton_7.setStyleSheet("border: 2px solid #8f8f91;\n"
+"\n"
+"    background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
+"                                      stop: 0 #f6f7fa, stop: 1 #dadbde);\n"
+"")
+        self.pushButton_7.setText("")
+        self.pushButton_7.setObjectName("pushButton_7")
+        self.pushButton_6 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_6.setGeometry(QtCore.QRect(220, 130, 41, 151))
+        self.pushButton_6.setStyleSheet("border: 2px solid #8f8f91;\n"
+"\n"
+"    background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
+"                                      stop: 0 #f6f7fa, stop: 1 #dadbde);\n"
+"")
+        self.pushButton_6.setText("")
+        self.pushButton_6.setObjectName("pushButton_6")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(530, 50, 58, 21))
+        self.label_2.setObjectName("label_2")
+        self.pushButton_23 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_23.setGeometry(QtCore.QRect(490, 130, 21, 91))
+        self.pushButton_23.setStyleSheet("border: 2px solid #858585;\n"
+"\n"
+"    background-color:black ;\n"
+"")
+        self.pushButton_23.setText("")
+        self.pushButton_23.setObjectName("pushButton_23")
+        self.pushButton_4 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_4.setGeometry(QtCore.QRect(140, 130, 41, 151))
+        self.pushButton_4.setStyleSheet("border: 2px solid #8f8f91;\n"
+"\n"
+"    background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
+"                                      stop: 0 #f6f7fa, stop: 1 #dadbde);\n"
+"")
+        self.pushButton_4.setText("")
+        self.pushButton_4.setObjectName("pushButton_4")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(20, 130, 41, 151))
+        self.pushButton.setStyleSheet("border: 2px solid #8f8f91;\n"
+"\n"
+"    background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
+"                                      stop: 0 #f6f7fa, stop: 1 #dadbde);\n"
+"")
+        self.pushButton.setText("")
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_25 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_25.setGeometry(QtCore.QRect(250, 360, 101, 71))
+        self.pushButton_25.setObjectName("pushButton_25")
+        self.pushButton_12 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_12.setGeometry(QtCore.QRect(460, 130, 41, 151))
+        self.pushButton_12.setStyleSheet("border: 2px solid #8f8f91;\n"
+"\n"
+"    background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
+"                                      stop: 0 #f6f7fa, stop: 1 #dadbde);\n"
+"")
+        self.pushButton_12.setText("")
+        self.pushButton_12.setObjectName("pushButton_12")
+        self.pushButton_18 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_18.setGeometry(QtCore.QRect(210, 130, 21, 91))
+        self.pushButton_18.setStyleSheet("border: 2px solid #858585;\n"
+"\n"
+"    background-color:black ;\n"
+"")
+        self.pushButton_18.setText("")
+        self.pushButton_18.setObjectName("pushButton_18")
+        self.pushButton_15 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_15.setGeometry(QtCore.QRect(50, 130, 20, 91))
+        self.pushButton_15.setStyleSheet("border: 2px solid #858585;\n"
+"\n"
+"    background-color:black ;\n"
+"")
+        self.pushButton_15.setText("")
+        self.pushButton_15.setObjectName("pushButton_15")
+        self.pushButton_13 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_13.setGeometry(QtCore.QRect(500, 130, 41, 151))
+        self.pushButton_13.setStyleSheet("border: 2px solid #8f8f91;\n"
+"\n"
+"    background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
+"                                      stop: 0 #f6f7fa, stop: 1 #dadbde);\n"
+"")
+        self.pushButton_13.setText("")
+        self.pushButton_13.setObjectName("pushButton_13")
+        self.spinBox_2 = QtWidgets.QSpinBox(self.centralwidget)
+        self.spinBox_2.setGeometry(QtCore.QRect(480, 50, 46, 26))
+        self.spinBox_2.setMinimum(-5)
+        self.spinBox_2.setMaximum(5)
+        self.spinBox_2.setObjectName("spinBox_2")
+        self.pushButton_11 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_11.setGeometry(QtCore.QRect(420, 130, 41, 151))
+        self.pushButton_11.setStyleSheet("border: 2px solid #8f8f91;\n"
+"\n"
+"    background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
+"                                      stop: 0 #f6f7fa, stop: 1 #dadbde);\n"
+"")
+        self.pushButton_11.setText("")
+        self.pushButton_11.setObjectName("pushButton_11")
+        self.pushButton_5 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_5.setGeometry(QtCore.QRect(180, 130, 41, 151))
+        self.pushButton_5.setStyleSheet("border: 2px solid #8f8f91;\n"
+"\n"
+"    background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
+"                                      stop: 0 #f6f7fa, stop: 1 #dadbde);\n"
+"")
+        self.pushButton_5.setText("")
+        self.pushButton_5.setObjectName("pushButton_5")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(290, 50, 58, 21))
+        self.label.setObjectName("label")
+        self.spinBox = QtWidgets.QSpinBox(self.centralwidget)
+        self.spinBox.setGeometry(QtCore.QRect(240, 50, 46, 26))
+        self.spinBox.setMinimum(-5)
+        self.spinBox.setMaximum(5)
+        self.spinBox.setObjectName("spinBox")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(60, 130, 41, 151))
+        self.pushButton_2.setStyleSheet("border: 2px solid #8f8f91;\n"
+"\n"
+"    background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
+"                                      stop: 0 #f6f7fa, stop: 1 #dadbde);\n"
+"")
+        self.pushButton_2.setText("")
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.checkBox = QtWidgets.QCheckBox(self.centralwidget)
+        self.checkBox.setGeometry(QtCore.QRect(20, 50, 99, 21))
+        self.checkBox.setObjectName("checkBox")
+        self.pushButton_27.raise_()
+        self.pushButton_14.raise_()
+        self.pushButton_13.raise_()
+        self.pushButton_12.raise_()
+        self.pushButton_11.raise_()
+        self.pushButton_10.raise_()
+        self.pushButton_9.raise_()
+        self.pushButton_8.raise_()
+        self.pushButton_7.raise_()
+        self.pushButton_6.raise_()
+        self.pushButton_5.raise_()
+        self.pushButton_4.raise_()
+        self.pushButton_3.raise_()
+        self.pushButton.raise_()
+        self.pushButton_2.raise_()
+        self.pushButton_22.raise_()
+        self.pushButton_16.raise_()
+        self.pushButton_19.raise_()
+        self.pushButton_21.raise_()
+        self.pushButton_17.raise_()
+        self.pushButton_24.raise_()
+        self.pushButton_20.raise_()
+        self.pushButton_26.raise_()
+        self.label_2.raise_()
+        self.pushButton_23.raise_()
+        self.pushButton_18.raise_()
+        self.pushButton_15.raise_()
+        self.spinBox_2.raise_()
+        self.label.raise_()
+        self.spinBox.raise_()
+        self.checkBox.raise_()
+        self.pushButton_25.raise_()
+        PianoForm.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(PianoForm)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 621, 24))
+        self.menubar.setObjectName("menubar")
+        self.menu = QtWidgets.QMenu(self.menubar)
+        self.menu.setObjectName("menu")
+        PianoForm.setMenuBar(self.menubar)
+        self.menubar.addAction(self.menu.menuAction())
+
+        self.retranslateUi(PianoForm)
+        QtCore.QMetaObject.connectSlotsByName(PianoForm)
+
+    def retranslateUi(self, PianoForm):
+        _translate = QtCore.QCoreApplication.translate
+        PianoForm.setWindowTitle(_translate("PianoForm", "MainWindow"))
+        self.pushButton_27.setText(_translate("PianoForm", "Стоп"))
+        self.pushButton_26.setText(_translate("PianoForm", "Прослушать"))
+        self.label_2.setText(_translate("PianoForm", "Тон"))
+        self.pushButton_25.setText(_translate("PianoForm", "Запись"))
+        self.label.setText(_translate("PianoForm", "Скорость"))
+        self.checkBox.setText(_translate("PianoForm", "Эхо"))
+        self.menu.setTitle(_translate("PianoForm", "Список мелодий"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    PianoForm = QtWidgets.QMainWindow()
+    ui = Ui_PianoForm()
+    ui.setupUi(PianoForm)
+    PianoForm.show()
+    sys.exit(app.exec_())
